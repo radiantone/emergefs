e3a4a95f1c0d832ee67ead119c8799274a49a69e
Darren Govoni <darren@rapidquest.ai>
2023-01-06 12:53:23 -0500

    Updates

---
+++ scripts/test.sh
@@ -0,0 +1,16 @@
+python emerge/example/widgets.py 
+python emerge/example/query.py 
+emerge methods /inventory/widget7
+emerge cat /inventory/widget1
+emerge help  /inventory/widget1
+emerge ls
+emerge ls -l
+emerge ls /inventory
+emerge ls -l /inventory
+emerge query /queries/query1
+emerge call -l /inventory/widget5 total_cost
+emerge call /inventory/widget5 total_cost
+emerge --debug call -l /inventory/widget5 total_cost
+emerge --debug call /inventory/widget3 total_cost
+emerge rm /inventory
+
