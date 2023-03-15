02ca43c9d29da3ed5c272b4514d11ba31adfe389
Darren Govoni <darren@rapidquest.ai>
2023-03-15 13:53:51 -0400

    Updated name

--- emerge/__version__.py
+++ emerge/__version__.py
@@ -5,10 +5,10 @@ __version__.py
 Information about the current version of the package.
 """
 
-__title__ = "emerge"
+__title__ = "emergefs"
 __description__ = "Emerge Computational Filesystem."
 __version__ = "0.0.1"
 __author__ = "Darren Govoni"
-__author_email__ = "darren@ontrenet.com"
+__author_email__ = "darren@elasticcode.ai"
 __license__ = "MIT License"
 __url__ = "https://github.com/radiantone/emerge"
